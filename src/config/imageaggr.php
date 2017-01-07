e74c07d0aefdc84afebcaf77a3faae85ff4ee37a
Boris <boris.dorofeev@gmail.com>
2017-01-07 14:30:00 +0600

    Зависимость от интервеншон и конфиг по умолчанию

--- src/config/imageaggr.php
+++ src/config/imageaggr.php
@@ -3,21 +3,21 @@
 return [
 
     'dirs' => [
-        'images' => 'images',
-        'tmp' => 'images/tmp',
-        'placeholders' => 'images/placeholders',
-        'crops' => 'images/crops',
-        'resizes' => 'images/resizes',
-        'resizestmp' => 'images/tmp/resizes'
+        'images' => '',
+        'tmp' => 'tmp',
+        'placeholders' => 'placeholders',
+        'crops' => 'crops',
+        'resizes' => 'resizes',
+        'resizestmp' => 'tmp/resizes'
     ],
 
     'paths' => [
-        'images' => 'images',
-        'tmp' => 'images/tmp',
-        'placeholders' => 'images/placeholders',
-        'crops' => 'images/crops',
-        'resizes' => 'images/resizes',
-        'resizestmp' => 'images/tmp/resizes'
+        'images' => '',
+        'tmp' => 'tmp',
+        'placeholders' => 'placeholders',
+        'crops' => 'crops',
+        'resizes' => 'resizes',
+        'resizestmp' => 'tmp/resizes'
     ]
 
 ];
