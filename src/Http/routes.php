14a6d1806a6dd258ac905b790bd2c94ee5eacd49
Boris <boris.dorofeev@gmail.com>
2017-02-13 18:10:56 +0600

    post в роутах для операций

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,10 +2,10 @@
 
 Route::group(['middleware' => 'auth', 'prefix' => 'adm'], function()
 {
-    Route::get('/image/test',    ['as' => 'clean_image',   'uses' => 'Interpro\ImageAggr\Http\ImageOperationController@testpage']);
-    Route::get('/image/clean',   ['as' => 'clean_image',   'uses' => 'Interpro\ImageAggr\Http\ImageOperationController@clean']);
-    Route::get('/image/phclean', ['as' => 'phclean_image', 'uses' => 'Interpro\ImageAggr\Http\ImageOperationController@cleanToPh']);
-    Route::get('/image/refresh', ['as' => 'refresh_image', 'uses' => 'Interpro\ImageAggr\Http\ImageOperationController@refresh']);
-    Route::get('/image/crop',    ['as' => 'crop_image',    'uses' => 'Interpro\ImageAggr\Http\ImageOperationController@crop']);
+    Route::get('/image/test',     ['as' => 'clean_image',   'uses' => 'Interpro\ImageAggr\Http\ImageOperationController@testpage']);
+    Route::post('/image/clean',   ['as' => 'clean_image',   'uses' => 'Interpro\ImageAggr\Http\ImageOperationController@clean']);
+    Route::post('/image/phclean', ['as' => 'phclean_image', 'uses' => 'Interpro\ImageAggr\Http\ImageOperationController@cleanToPh']);
+    Route::post('/image/refresh', ['as' => 'refresh_image', 'uses' => 'Interpro\ImageAggr\Http\ImageOperationController@refresh']);
+    Route::post('/image/crop',    ['as' => 'crop_image',    'uses' => 'Interpro\ImageAggr\Http\ImageOperationController@crop']);
     Route::post('/image/upload',  ['as' => 'upload_image',  'uses' => 'Interpro\ImageAggr\Http\ImageOperationController@upload']);
 });
